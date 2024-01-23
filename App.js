fa8de755d5c61b37c7ecd00914ec9d9929920a00
KarlT4 <kaarlojuuolen@hotmail.com>
2024-01-23 22:09:37 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+    const [numero1, setNumero1] = useState('');
+    const [numero2, setNumero2] = useState('');
+    const [tulos, setTulos] = useState(null);
+
+  const Plus = () => {
+      const summa = Number(numero1) + Number(numero2);
+      setTulos(`Summa: ${summa}`);
+  };
+
+  const Miinus = () => {
+      const erotus = Number(numero1) - Number(numero2);
+      setTulos(`Erotus: ${erotus}`); 
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Syötä ensimmäinen numero"
+        keyboardType="numeric"
+        value={numero1}
+        onChangeText={(text) => setNumero1(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Syötä toinen numero"
+        keyboardType="numeric"
+        value={numero2}
+        onChangeText={(text) => setNumero2(text)}
+      />
+      <Button title="+" onPress={Plus} />
+      <Button title="-" onPress={Miinus} />
+      <Text>{tulos}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +47,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    width: 300,
+    margin: 20,
+    padding: 20,
+  },
+  tulos: {
+    fontSize: 18,
+    margin: 20,
+  },
+
 });
